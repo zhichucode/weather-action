11f3f4ae4fe2e6278c5b5caa252581a70d8fa059
Albert Zhang <43780800+zhichucode@users.noreply.github.com>
2021-12-18 14:49:11 +0800

    12

--- weather.sh
+++ weather.sh
@@ -13,5 +13,5 @@ UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Ge
 #  -o result.html \
 #  wttr.in/$CITY?format=4\&$UNIT
 
-curl -o result.html -H "Accept-Language: $LANGUAGE" wttr.in/Shenyang?format=4
-echo -e "\n感谢你收看今天的天气预报\n" >> result.html
+curl -o result.html -H "Accept-Language: $LANGUAGE" wttr.in/Shenyang?format=4\&$UNIT
+echo -e "\n感谢你收看今天的天气预报\n想要退订吗，想得美\n" >> result.html
